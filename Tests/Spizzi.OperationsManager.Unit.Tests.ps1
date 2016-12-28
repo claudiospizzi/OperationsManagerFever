a19c337197aed2b75f7c6006f71f61b3291f38fe
Claudio Spizzi <claudio.spizzi@live.com>
2016-12-28 17:12:29 +0100

    Update tests

--- Tests/Spizzi.OperationsManager.Unit.Tests.ps1
+++
@@ -1,6 +0,0 @@
-
-$ModuleName  = ($PSScriptRoot | Split-Path | Split-Path -Leaf)
-$ProjectRoot = ($PSScriptRoot | Split-Path)
-
-Remove-Module -Name $ModuleName -Verbose:$false -ErrorAction SilentlyContinue -Force
-Import-Module "$ProjectRoot\$ModuleName" -Verbose:$false -ErrorAction Stop -Force
